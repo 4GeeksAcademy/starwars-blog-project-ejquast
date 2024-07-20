c17241d94f6cbc297e4333963fecd325af7d3cdd
EQ <167933418+ejquast@users.noreply.github.com>
2024-07-20 01:02:58 +0000

    first save

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="mt-5">
+		<h1>Characters</h1>
+		<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
+			<img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		<h1>Planets</h1>
+		<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
+			<img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
 	</div>
 );
