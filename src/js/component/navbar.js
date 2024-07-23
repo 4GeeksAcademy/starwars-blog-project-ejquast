bc1a8a1e13832f90f1d6ddd508d66e6c8f1c8cde
EQ <167933418+ejquast@users.noreply.github.com>
2024-07-23 00:26:45 +0000

    second save

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,12 +4,17 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
+			<Link to="/" className="navbar-brand ms-5">
+          		<img src="https://seeklogo.com/images/S/Star_Wars-logo-97DD55947B-seeklogo.com.png" alt="Logo" className="img-fluid" style={{ maxWidth: '100px' }} />
+        	</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<div class="btn-group me-5" role="group">
+						<button type="button" class="btn btn-primary dropdown-toggle btn-lg" data-bs-toggle="dropdown" aria-expanded="false">Favorites</button>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#">Favorite</a></li>
+						</ul>
+					</div>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,23 +3,39 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="mt-5">
-		<h1>Characters</h1>
-		<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
-			<img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<a href="#" className="btn btn-primary">Go somewhere</a>
+	<div className="ms-5">
+		<h1 className="mt-5 text-danger">Characters</h1>
+		<div className="mt-3 card-container">
+			<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
+				<img src="https://picsum.photos/300/200" className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-outline-primary">Learn more!</a>
+					<a href="#" className="btn btn-outline-primary"><i class="fa-regular fa-heart"></i></a>
+				</div>
 			</div>
 		</div>
-		<h1>Planets</h1>
-		<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
-			<img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<a href="#" className="btn btn-primary">Go somewhere</a>
+		<h1 className="mt-5 text-danger">Starships</h1>
+		<div className="mt-3 card-container">
+			<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
+				<img src="https://picsum.photos/300/200" className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-outline-primary">Learn more!</a>
+				</div>
+			</div>
+		</div>
+		<h1 className="mt-5 text-danger">Planets</h1>
+		<div className="mt-3 card-container">
+			<div className="card" style={{minWidth: "18rem",width: "18rem"}}>
+				<img src="https://picsum.photos/300/200" className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-outline-primary">Learn more!</a>
+				</div>
 			</div>
 		</div>
 	</div>
