f12ed025e0e9615157c52d9abb586d379483e4d3
EQ <167933418+ejquast@users.noreply.github.com>
2024-07-29 23:25:03 +0000

    saving work in progress

--- src/js/component/Characters.js
+++ src/js/component/Characters.js
@@ -5,27 +5,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Characters = () => {
-    const { store, actions } = useContext(Context);
+    // const { store, actions } = useContext(Context);
 	
 	return (
-	<div className="ms-5">
-		<h1 className="mt-5 text-danger">Characters</h1>
-		<div className="mt-3 card-container">
-			<div className="card" style={{minWidth: "400px",width: "400px"}}>
-				<img src="https://picsum.photos/400/200" className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title mb-3">Name</h5>
-					<p className="card-text mb-0">Gender:</p>
-					<p className="card-text mb-0">Hair Color:</p>
-					<p className="card-text">Eye Color:</p>
-					<div className="d-flex justify-content-between">
-						<a href="#" className="btn btn-outline-primary">Learn more!</a>
-						<a href="#" className="btn btn-outline-warning"><i class="fa-regular fa-heart"></i></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div className="mt-3 card-container">
+        <div className="card" style={{minWidth: "400px",width: "400px"}}>
+            <img src="https://picsum.photos/400/200" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title mb-3">Name</h5>
+                <p className="card-text mb-0">Gender:</p>
+                <p className="card-text mb-0">Hair Color:</p>
+                <p className="card-text">Eye Color:</p>
+                <div className="d-flex justify-content-between">
+                    <a href="#" className="btn btn-outline-primary">Learn more!</a>
+                    <a href="#" className="btn btn-outline-warning"><i className="fa-regular fa-heart"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 	);
 
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,27 +3,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Characters } from "../component/Characters";
 
 export const Home = () => {
 	
 	return (
 	<div className="ms-5">
 		<h1 className="mt-5 text-danger">Characters</h1>
-		<div className="mt-3 card-container">
-			<div className="card" style={{minWidth: "400px",width: "400px"}}>
-				<img src="https://picsum.photos/400/200" className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title mb-3">Name</h5>
-					<p className="card-text mb-0">Gender:</p>
-					<p className="card-text mb-0">Hair Color:</p>
-					<p className="card-text">Eye Color:</p>
-					<div className="d-flex justify-content-between">
-						<a href="#" className="btn btn-outline-primary">Learn more!</a>
-						<a href="#" className="btn btn-outline-warning"><i className="fa-regular fa-heart"></i></a>
-					</div>
-				</div>
-			</div>
-		</div>
+		< Characters />
 		<h1 className="mt-5 text-danger">Starships</h1>
 		<div className="mt-3 card-container">
 			<div className="card" style={{minWidth: "400px",width: "400px"}}>
